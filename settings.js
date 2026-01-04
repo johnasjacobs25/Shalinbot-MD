b649d37dda8514573a0f9a92e163ecc376353e3c
johnasjacobs25 <johnasjacobs25@gmail.com>
2026-01-04 14:35:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Shalin Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Shalin Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254717768930', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
